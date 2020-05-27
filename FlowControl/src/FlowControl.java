f21f2f6dbc424bbd1c1223a46da1cf5dd272a2d4
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2020-05-27 16:53:35 -0300

    Trabalhando com else if

--- FlowControl/src/FlowControl.java
+++ FlowControl/src/FlowControl.java
@@ -3,12 +3,17 @@ public class FlowControl {
 	public static void main(String[] args) {
 		int age = 20;
 				
-		if( age >= 18 ){
-			System.out.println("Over eighteen!");
+		System.out.println("AGE: " + age);
+		System.out.print("VOTE STATUS: ");
+		if( age < 16 ){
+			System.out.println("Can not vote!");			
+		}
+		else if( age < 18 || age >= 70  ) {
+			System.out.println("Optional vote!");
 		}
 		else
 		{
-			System.out.println("Under age");
+			System.out.println("Obrigatory vote!");
 		}
 	}
 }
