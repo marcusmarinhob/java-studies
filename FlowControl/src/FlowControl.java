d234c3faba56623767f1f66de967cf85eed9795a
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2020-05-27 16:29:05 -0300

    Trabalhando com if e else

---
+++ FlowControl/src/FlowControl.java
@@ -0,0 +1,14 @@
+
+public class FlowControl {
+	public static void main(String[] args) {
+		int age = 20;
+				
+		if( age >= 18 ){
+			System.out.println("Over eighteen!");
+		}
+		else
+		{
+			System.out.println("Under age");
+		}
+	}
+}
