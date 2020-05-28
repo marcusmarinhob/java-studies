f4349e1eb56bd70eee72f4e494e9992dd056463b
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2020-05-27 22:24:42 -0300

    Trabalhando com do-while

--- FlowControl/src/FlowControl4.java
+++ FlowControl/src/FlowControl4.java
@@ -3,8 +3,18 @@ public class FlowControl4 {
 	public static void main(String[] args) {
 		int count = 0;
 		
+		System.out.println("Begin of while!");
 		while(count < 10) {
 			System.out.println("Count = " + count++);			
 		}
+		System.out.println("End of while!");
+		System.out.println();
+		
+		
+		System.out.println("Begin of do-while!");
+		do{
+			System.out.println("Count = " + count--);			
+		} while(count > 0);
+		System.out.println("End of do-while!");
 	}
 }
