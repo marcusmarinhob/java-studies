95099641cf3f49880439fcf6cd768cd8c6e8e5da
marcusmarinhob <marcus.bezerra@virtus.ufcg.edu.br>
2020-05-27 22:51:15 -0300

    Trabalhando com Arrays de Strings

---
+++ Arrays/src/Arrays2.java
@@ -0,0 +1,14 @@
+
+public class Arrays2 {
+	public static void main(String[] args) {
+		String [] names = new String[3];
+		
+		names[0] = "Naruto";
+		names[1] = "Goku";
+		names[2] = "Luffy";
+		
+		for(int index=0; index < names.length; index++) {
+			System.out.println(names[index]);
+		}
+	}
+}
