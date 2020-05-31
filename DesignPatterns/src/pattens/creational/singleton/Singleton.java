3548df09884736bd5900cb902624115f4c57e035
marcusmarinhob <marcus.bezerra@ee.ufcg.edu.br>
2020-05-31 13:57:21 -0300

    Trabalhando com o padrão Singleton

---
+++ DesignPatterns/src/pattens/creational/singleton/Singleton.java
@@ -0,0 +1,18 @@
+package pattens.creational.singleton;
+
+public class Singleton {
+	// The class variable is null if no instance is instantiated
+	private static Singleton uniqueInstance = null;
+	
+	private Singleton() {
+
+	}
+	
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		
+		return uniqueInstance;
+	}
+}
